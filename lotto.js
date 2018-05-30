0eaaacc44955fa60683adc3032c05dc887135ddb
sejungkim <smilesejung@gmail.com>
2018-05-30 16:42:21 +0900

    Add function to check valid of 'money' value

--- lotto.js
+++ lotto.js
@@ -1,15 +1,29 @@
 'use strict';
 
+const lottoSetList = [];
+
 function buyLottos(money) {
-  const numberOfLottos = getNumberOfLottos(money);
-  console.log(numberOfLottos);
+  const lottoPrice = 1000;
+
+  checkMoneyValid(money, lottoPrice);
+
+  const numberOfLottos = getNumberOfLottos(money, lottoPrice);
+  console.log(`>> 로또 ${numberOfLottos}개를 발행했습니다.`);
 }
 
-function getNumberOfLottos(money) {
-  const lottoPrice = 1000;
+function checkMoneyValid(money, lottoPrice) {
+  if (!Number.isInteger(money)) {
+    throw `>> [!] 잘못된 입력입니다.`;
+  }
+  if (money < lottoPrice) {
+    throw `>> [!] ${lottoPrice}원 이상의 금액을 입력해주세요.`;
+  }
+}
+
+function getNumberOfLottos(money, lottoPrice) {
   const numberOfLottos = Math.floor(money / lottoPrice);
   return numberOfLottos;
 }
 
 // run
-buyLottos(1000);
+buyLottos(5000);
