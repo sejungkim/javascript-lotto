b96413eec1feb1a0a0e22844d95af18d847bfc8b
sejungkim <smilesejung@gmail.com>
2018-06-01 18:08:18 +0900

    Complete the buyLottos function
    Added the function of
    - create lotto number
    - create list of lottos

--- lotto.js
+++ lotto.js
@@ -1,14 +1,19 @@
 'use strict';
 
-const lottoSetList = [];
+let lottoSetList = [];
 
 function buyLottos(money) {
   const lottoPrice = 1000;
 
   checkMoneyValid(money, lottoPrice);
 
+  // Number of lottos for money
   const numberOfLottos = getNumberOfLottos(money, lottoPrice);
   console.log(`>> 로또 ${numberOfLottos}개를 발행했습니다.`);
+
+  // Create list of lottos
+  lottoSetList = getLottoSetList(numberOfLottos);
+  lottoSetList.forEach(lottoSet => console.log(lottoSet));
 }
 
 function checkMoneyValid(money, lottoPrice) {
@@ -23,6 +28,33 @@ function checkMoneyValid(money, lottoPrice) {
 function getNumberOfLottos(money, lottoPrice) {
   const numberOfLottos = Math.floor(money / lottoPrice);
   return numberOfLottos;
+}
+
+function getLottoSet() {
+  const lottoNumRange = { min: 1, max: 45 };
+  const lottoLength = 6;
+  const lottoSet = [];
+
+  while (lottoSet.length < lottoLength) {
+    let lottoNumber = getRandomNumber(lottoNumRange.min, lottoNumRange.max);
+    if (!lottoSet.includes(lottoNumber)) lottoSet.push(lottoNumber);
+  }
+
+  lottoSet.sort((num1, num2) => num1 - num2);
+
+  return lottoSet;
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * max + min);
+}
+
+function getLottoSetList(numberOfLottos) {
+  const lottoSetList = [];
+  while (lottoSetList.length < numberOfLottos) {
+    lottoSetList.push(getLottoSet());
+  }
+  return lottoSetList;
 }
 
 // run
